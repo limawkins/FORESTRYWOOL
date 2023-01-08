b1d2e892bef7a8c3b83f74e06c46a3b14ff3327b
limawkins <102392421+limawkins@users.noreply.github.com>
2023-01-08 03:50:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-
+let links = document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        targetID = element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView({
+            behavior:'smooth',
+            block:'start'
+        })
+    })
+})
 $(function() {
     let header = $('.header');
      
