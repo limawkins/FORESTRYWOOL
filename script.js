db80257cee7fd4d4b335dcd6b998d9fbf4be5b26
limawkins <102392421+limawkins@users.noreply.github.com>
2023-01-08 03:34:20 +0300

    1

---
+++ script.js
@@ -0,0 +1,30 @@
+
+$(function() {
+    let header = $('.header');
+     
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header_fixed');
+      } else {
+       header.removeClass('header_fixed');
+      }
+    });
+   });
+   $(function() {
+    let header = $('.header');
+    let headerHeight = header.height(); // вычисляем высоту шапки
+     
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header_fixed');
+       $('body').css({
+          'paddingTop': headerHeight+'px' // делаем отступ у body, равный высоте шапки
+       });
+      } else {
+       header.removeClass('header_fixed');
+       $('body').css({
+        'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+       })
+      }
+    });
+   });
